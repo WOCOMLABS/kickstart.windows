55e537dceba896b6ac73c8295613baaa97492569
Jazielsama <eddy.velazquez@wocom-labs.com>
2024-11-29 16:37:17 -0800

    Update install_git.ps1

--- scripts/install_git.ps1
+++ scripts/install_git.ps1
@@ -1,16 +1,27 @@
 if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+
     Write-Host "Installing Git..."
-    winget install --id Git.Git -e --source winget
-    $userName = Read-Host -Prompt "Enter your Git user name for your windows machine"
-    $userEmail = Read-Host -Prompt "Enter your Git user email for your windows machine"
-    Write-Host "Configuring Git..."
-    git config --global user.name "$userName"
-    git config --global user.email "$userEmail"
-    git config --global init.defaultBranch main
-    git config --global --unset credential.helper
-    git config --global credential.helper store
+    Start-Process -FilePath "winget" -ArgumentList "install --id Git.Git -e --source winget" -Wait
+
+    # Actualizar PATH en la sesión actual
+    $env:Path = [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::Machine) + ";" + 
+                [System.Environment]::GetEnvironmentVariable("Path", [System.EnvironmentVariableTarget]::User)
+
+    # Verificar si git está disponible
+    if (-not (Get-Command git -ErrorAction SilentlyContinue)) {
+        Write-Host "Git did not install correctly or is not in the PATH. Please check manually."
+        exit
+    }
 }
 
+$userName = Read-Host -Prompt "Enter your Git user name for your windows machine"
+$userEmail = Read-Host -Prompt "Enter your Git user email for your windows machine"
+Write-Host "Configuring Git..."
+git config --global user.name "$userName"
+git config --global user.email "$userEmail"
+git config --global init.defaultBranch mail
+git config --global --unset credential.helper
+git config --global credential.helper store
 Write-Host "Installing GitHub CLI..."
 winget install -e --id GitHub.cli
 Write-Host "Installing Starship..."
